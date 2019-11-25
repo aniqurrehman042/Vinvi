c24d767092a8fc3754c068d7f14e48742fdd7563
Aniq <aniqurrehman042@gmail.com>
2019-11-25 22:47:40 +0500

    added create new visiting card activity

--- app/src/main/java/com/example/vinvi/ui/CreateNewVisitingCard.java
+++ app/src/main/java/com/example/vinvi/ui/CreateNewVisitingCard.java
@@ -2,18 +2,15 @@ package com.example.vinvi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.vinvi.R;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateNewVisitingCard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(this, CreateNewVisitingCard.class));
+        setContentView(R.layout.activity_create_new_visiting_card);
     }
 }
