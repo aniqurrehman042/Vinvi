30bd1c891ee56b66b6c3793c8639d61672a873bd
Aniq <aniqurrehman042@gmail.com>
2019-11-26 02:40:54 +0500

    added settings activity

---
+++ app/src/main/java/com/example/vinvi/ui/Settings.java
@@ -0,0 +1,16 @@
+package com.example.vinvi.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vinvi.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
