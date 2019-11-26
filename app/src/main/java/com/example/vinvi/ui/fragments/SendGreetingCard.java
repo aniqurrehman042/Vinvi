afecb12f21403fd4af08f8a67b119fdb5979f781
Aniq <aniqurrehman042@gmail.com>
2019-11-26 23:34:38 +0500

    added send greeting card fragment

---
+++ app/src/main/java/com/example/vinvi/ui/fragments/SendGreetingCard.java
@@ -0,0 +1,29 @@
+package com.example.vinvi.ui.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vinvi.R;
+
+public class SnedGreetingCard extends Fragment {
+
+
+    public SnedGreetingCard() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sned_greeting_card, container, false);
+    }
+
+}
